9f885da2a4403876cf5e8ce5558d3f9ba6d5ea12
zackcloud35 <zackcloud@yahoo.com>
2025-06-05 09:24:57 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1 style={{ color: "#00FF00" }}>VaultVerse is LIVE!</h1>
+    </div>
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNj2oKEKJo7Yi9u_cHiUEGTgA79X0KqHM",
+  authDomain: "vaultverse-true-test.firebaseapp.com",
+  projectId: "vaultverse-true-test",
+  storageBucket: "vaultverse-true-test.firebasestorage.app",
+  messagingSenderId: "668740714004",
+  appId: "1:668740714004:web:046936bc6d6ffc757eb101"
+};
+
+const app = initializeApp(firebaseConfig);
